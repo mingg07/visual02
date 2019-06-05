91f0d39542bd118ce5216df549b69ffb6c188614
mingg07 <51304179+mingg07@users.noreply.github.com>
2019-06-05 12:14:22 +0900

    Add files via upload

---
+++ A005_Calc/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace A005
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNo1 = new System.Windows.Forms.TextBox();
+            this.txtNo2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼편지체", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(90, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "숫자1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("휴먼편지체", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(90, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "숫자2";
+            // 
+            // txtNo1
+            // 
+            this.txtNo1.Font = new System.Drawing.Font("휴먼편지체", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtNo1.Location = new System.Drawing.Point(191, 39);
+            this.txtNo1.Multiline = true;
+            this.txtNo1.Name = "txtNo1";
+            this.txtNo1.Size = new System.Drawing.Size(100, 61);
+            this.txtNo1.TabIndex = 2;
+            this.txtNo1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNo1.TextChanged += new System.EventHandler(this.txtNo1_TextChanged);
+            // 
+            // txtNo2
+            // 
+            this.txtNo2.Font = new System.Drawing.Font("휴먼편지체", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtNo2.Location = new System.Drawing.Point(191, 125);
+            this.txtNo2.Multiline = true;
+            this.txtNo2.Name = "txtNo2";
+            this.txtNo2.Size = new System.Drawing.Size(100, 64);
+            this.txtNo2.TabIndex = 3;
+            this.txtNo2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.White;
+            this.btnPlus.Location = new System.Drawing.Point(28, 219);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 64);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.White;
+            this.btnMinus.Location = new System.Drawing.Point(109, 219);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 64);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.BackColor = System.Drawing.Color.White;
+            this.btnTimes.Location = new System.Drawing.Point(191, 218);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(75, 65);
+            this.btnTimes.TabIndex = 6;
+            this.btnTimes.Text = "*";
+            this.btnTimes.UseVisualStyleBackColor = false;
+            this.btnTimes.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.White;
+            this.btnDivide.Location = new System.Drawing.Point(273, 217);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 66);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("휴먼편지체", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblResult.Location = new System.Drawing.Point(62, 309);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 28);
+            this.lblResult.TabIndex = 8;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("휴먼편지체", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.Location = new System.Drawing.Point(191, 301);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 47);
+            this.txtResult.TabIndex = 9;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(400, 398);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtNo2);
+            this.Controls.Add(this.txtNo1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "사칙계산기 by 최민지";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNo1;
+        private System.Windows.Forms.TextBox txtNo2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ A005_Calc/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A005
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(txtNo1.Text == "" || txtNo2.Text=="")
+            {
+                MessageBox.Show("숫자 두 개를 다 입력하세요.");
+                return;
+            }
+            //숫자1에서 숫자를 가져온다.
+            double label1 = double.Parse(txtNo1.Text);
+
+            //숫자2에서 숫자를 가져온다.
+            double label2 = double.Parse(txtNo2.Text);
+
+            //계산을 한다.
+            lblResult.Text = "(*) 결과";
+
+            double result = 0;
+            //결과창에 보여준다.
+            result = label1 * label2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtNo1.Text == "" || txtNo2.Text == "")
+            {
+                MessageBox.Show("숫자 두 개를 다 입력하세요.");
+                return;
+            }
+            //숫자1에서 숫자를 가져온다.
+            double label1 = double.Parse(txtNo1.Text);
+
+            //숫자2에서 숫자를 가져온다.
+            double label2 = double.Parse(txtNo2.Text);
+
+            //계산을 한다.
+            lblResult.Text = "(/) 결과";
+
+            double result = 0;
+            //결과창에 보여준다.
+            result = label1 / label2;
+            txtResult.Text = result.ToString("0.00");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNo1.Text == "" || txtNo2.Text == "")
+            {
+                MessageBox.Show("숫자 두 개를 다 입력하세요.");
+                return;
+            }
+            //숫자1에서 숫자를 가져온다.
+            double label1 = double.Parse(txtNo1.Text);
+
+            //숫자2에서 숫자를 가져온다.
+            double label2 = double.Parse(txtNo2.Text);
+
+            //계산을 한다.
+            lblResult.Text = "(+) 결과";
+
+            double result = 0;
+            //결과창에 보여준다.
+            result = label1+label2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtNo1.Text == "" || txtNo2.Text == "")
+            {
+                MessageBox.Show("숫자 두 개를 다 입력하세요.");
+                return;
+            }
+            //숫자1에서 숫자를 가져온다.
+            double label1 = double.Parse(txtNo1.Text);
+
+            //숫자2에서 숫자를 가져온다.
+            double label2 = double.Parse(txtNo2.Text);
+
+            //계산을 한다.
+            lblResult.Text = "(-) 결과";
+
+            double result = 0;
+            //결과창에 보여준다.
+            result = label1 - label2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void txtNo1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
