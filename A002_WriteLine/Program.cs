a3674d97b0de663467b72da91524c7ea35f5c404
mingg07 <51304179+mingg07@users.noreply.github.com>
2019-06-05 12:11:13 +0900

    Add files via upload

---
+++ A002_WriteLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A002_WriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+
+            Console.WriteLine(x.ToString() + ", " + y.ToString());   // or (x + ", " + y)
+            // printf("%d %d", x, y);
+            Console.WriteLine("{0}, {1}", x, y);
+            Console.WriteLine($"{x}, {y}");   // $는 변수 값으로 인식
+
+            string s = string.Format($"{x}, {y}");
+            Console.WriteLine(s);
+
+            string t = string.Format("{0}, {1}", x, y);
+            Console.WriteLine(t);
+        }
+    }
+}
